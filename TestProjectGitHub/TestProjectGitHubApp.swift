d4501f2f08104beec02f795ec2ed6895b1598bfb
Diyor <dierruzmatov@gmail.com>
2026-01-15 16:16:52 +0500

    Initial Commit

---
+++ TestProjectGitHub/TestProjectGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectGitHubApp.swift
+//  TestProjectGitHub
+//
+//  Created by Shahriyor on 15/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
